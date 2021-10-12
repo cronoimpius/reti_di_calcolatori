bef05fce6005261c116ea9cdca72c2f8da035fa9
cronoimpius <72434034+cronoimpius@users.noreply.github.com>
2021-10-12 19:11:37 +0200

    Add files via upload
    consigned files

--- es0/c/consumatore.c
+++ es0/c/consumatore.c
@@ -3,49 +3,56 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <stdbool.h>
 #define MAX_STRING_LENGTH 256
 
 // consumatore.c e' un filtro
-int main(int argc, char* argv[]){
-
-	char *file_in, read_char, *filter;//filter rappresenta la stringa di caratteri che vogliamo eliminare dal file
-	int nread,i, fd,fil_len;//fil_len è la lunghezza della stringa di caratteri da eliminare
-	bool match;
+int main(int argc, char* argv[])
+{
+	char *ch, *file_in, read_char, buf[MAX_STRING_LENGTH];
+	int nread, fd, match;
 	
 	//controllo numero argomenti
-	if (argc == 3 ){
-		file_in=argv[2];
-		fd=open(file_in,O_RDONLY);
-		if (fd<0){
-                	perror("P0: Impossibile aprire il file.");
-                	exit(2);
-        	}
-	}
-	else if (argc ==2){ 
-		fd=STDIN_FILENO;
-	}else{
-		perror(" numero di argomenti sbagliato"); 
-		exit(1);
+	if (argc != 3 && argc !=2)
+	{ 
+		perror(" numero di argomenti sbagliato"); exit(1);
 	} 
 	
-	filter = argv[1];
-	fil_len=strlen(filter);
-	
-	while(nread = read(fd, &read_char, sizeof(char))) /* Fino ad EOF*/{
-		if (nread >= 0){
-			match=false;
-			for (i=0; i<fil_len && !match; i++)
-				if(read_char==filter[i])
-					match=true;
-				if(!match)
-					putchar(read_char);
-		}else{
+	if(argc == 3)
+	{
+		file_in = argv[2];
+		fd = open(file_in, O_RDONLY);
+		if (fd<0){
+			perror("P0: Impossibile aprire il file.");
+			exit(2);
+		}
+	}
+	else 
+		fd=0;
+
+	while(nread = read(fd, &read_char, sizeof(char))) /* Fino ad EOF*/
+	{
+		//ch=NULL;
+		match=0;
+		if (nread >= 0) 
+		{
+			/*ch=strstr(argv[1], &read_char);
+			if(ch)
+				match=1;*/
+			for(int i = 0; argv[1][i]!='\0'; i++)
+			{
+            			if(read_char == argv[1][i])
+					match=1;
+			}
+		}
+
+		else
+		{
 			printf("(PID %d) impossibile leggere dal file %s", getpid(), file_in);
 			perror("Errore!");
 			close(fd);
 			exit(3);
 		}
+		if(match==0) putchar(read_char);
 	}
 	close(fd);
 }
