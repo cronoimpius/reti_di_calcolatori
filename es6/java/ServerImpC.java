49ff7423fbdcaeba06652c0625e4262031ded82c
cronoimpius <72434034+cronoimpius@users.noreply.github.com>
2021-11-22 15:22:16 +0100

    Update ServerImpC.java

--- es6/java/ServerImpC.java
+++ es6/java/ServerImpC.java
@@ -5,6 +5,8 @@ import java.rmi.server.UnicastRemoteObject;
 import java.rmi.registry.*;
 
 public class ServerImpC extends UnicastRemoteObject implements RemOp {
+	
+	private static final long serialVersionUID=1L;
 
 	// Costruttore
 		public ServerImpC() throws RemoteException {
@@ -12,7 +14,7 @@ public class ServerImpC extends UnicastRemoteObject implements RemOp {
 		}
 
 
-		public int conta_righe(String nomeF, int numw)throws IOException{
+		public int conta_righe(String nomeF, int numw)throws RemoteException{
         int res=0,i=0,wc=0;
         BufferedReader buff;
         char ch;
@@ -51,7 +53,7 @@ public class ServerImpC extends UnicastRemoteObject implements RemOp {
 
     }
 
-    public String elimina_riga(String nomeF, int nl)throws IOException{
+    public String elimina_riga(String nomeF, int nl)throws RemoteException{
         int att=1,i; //il file inizia a contare dalla linea 1
         String newN = nomeF.substring(0,(nomeF.length()-4))+"_mod.txt";
         String res;
